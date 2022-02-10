42e0924dabda1f5c238f3327d1b316cb8ac623e8
Francesco Maestrelli <framae26@gmail.com>
2022-02-10 17:02:01 +0100

    qualcosa non torna nel carousel

--- main.js
+++ main.js
@@ -4,27 +4,27 @@ const app = new Vue (
         data: {
             items: [
                         {
-                            photo: 'img/01.jpg',
+                            url: 'img/01.jpg',
                             title: 'Svezia',
                             text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
                         },
                         {
-                            photo: 'img/02.jpg',
+                            url: 'img/02.jpg',
                             title: 'Svizzera',
                             text: 'Lorem ipsum.'
                         },
                         {
-                            photo: 'img/03.jpg',
+                            url: 'img/03.jpg',
                             title: 'Gran Bretagna',
                             text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
                         },
                         {
-                            photo: 'img/04.jpg',
+                            url: 'img/04.jpg',
                             title: 'Germania',
                             text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.'
                         },
                         {
-                            photo: 'img/05.jpg',
+                            url: 'img/05.jpg',
                             title: 'Paradise',
                             text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.'
                         }
@@ -32,6 +32,23 @@ const app = new Vue (
             onDisplay: 0       
         },
         methods: {
+            previous() {
+                if (this.onDisplay == 0) {
+                    this.onDisplay = this.items.lenght - 1;
+                } else {
+                    this.onDisplay--
+                }
+            },
+
+            next() {
+                if (this.onDisplay == this.items.lenght - 1) {
+                    this.onDisplay = 0;
+                } else {
+                        this.onDisplay++;
+                    }
+            }
 
         }
-    });
+    }
+    
+);
